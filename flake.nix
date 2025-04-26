16076407de20e00c70fe116917630f80e7755f0b
nttis <slush-gulf-rural@duck.com>
2025-04-26 20:58:48 +0700

    syntax: fix CSS

--- flake.nix
+++ flake.nix
@@ -1,40 +1,19 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    snowfall-lib = {
-      url = "github:snowfallorg/lib";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = inputs:
-    inputs.snowfall-lib.mkFlake {
-      inherit inputs;
-      src = ./.;
-
-      channels-config = {
-        allowUnfree = true;
+  outputs = inputs: let
+    forAllSystems = inputs.nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = import inputs.nixpkgs {
+        system = system;
       };
-
-      # Configure Snowfall Lib, all of these settings are optional.
-      snowfall = {
-        # Tell Snowfall Lib to look in the `./nix/` directory for your
-        # Nix files.
-        root = ./.;
-
-        # Choose a namespace to use for your flake's packages, library,
-        # and overlays.
-        namespace = "blog";
-
-        # Add flake metadata that can be processed by tools like Snowfall Frost.
-        meta = {
-          # A slug to use in documentation when displaying things like file paths.
-          name = "Blog";
-
-          # A title to show for your flake, typically the name.
-          title = "Truly the blog of all time";
-        };
+    in {
+      default = pkgs.mkShellNoCC {
+        packages = with pkgs; [zola];
       };
-    };
+    });
+  };
 }
